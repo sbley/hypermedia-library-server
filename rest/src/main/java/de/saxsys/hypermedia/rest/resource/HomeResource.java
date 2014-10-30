d06f307b50305196ebfe2c170e8fb1450740fbd8
Stefan Bley <stefan.bley@saxsys.de>
2014-10-30 08:31:35 +0100

    HomeResource with CacheControl

--- rest/src/main/java/de/saxsys/hypermedia/rest/resource/HomeResource.java
+++ rest/src/main/java/de/saxsys/hypermedia/rest/resource/HomeResource.java
@@ -9,6 +9,7 @@ import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
@@ -42,7 +43,13 @@ public class HomeResource {
                                         .toString()
                                         + "?q={query}")
                         .withNamespace(LinkRelations.NAMESPACE, LinkRelations.NAMESPACE_HREF))
+                .cacheControl(cc())
+                .build();
+    }
 
-        .build();
+    private CacheControl cc() {
+        CacheControl cc = new CacheControl();
+        cc.setMaxAge(10); // 10 seconds
+        return cc;
     }
 }
